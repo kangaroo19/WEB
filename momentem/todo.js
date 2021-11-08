1a963f5280cd1106beb794d5d231a3dc4c494eaf
kangaroo19 <86513078+kangaroo19@users.noreply.github.com>
2021-11-08 23:12:19 +0900

    12

--- momentem/todo.js
+++ momentem/todo.js
@@ -11,17 +11,16 @@ function saveToDos(){
 
 function deleteToDo(event){
     const li=event.target.parentElement;//삭제하고 싶은 li
-    console.log(typeof li.id);
     li.remove();//요소 삭제하는 메소드
     toDos=toDos.filter(toDo=>toDo.id!=parseInt(li.id));//toDo.id는 int li.id는 string
     saveToDos();
 }
 
-function paintToDo(newToDo){
+function paintToDo(newToDoObj){
     const li=document.createElement("li"); //문서에 li생성
-    li.id=newToDo.id; //html li 의 id를 객체로 전달받은 값(newToDoObj)의 id(date.now)로 지정
+    li.id=newToDoObj.id; //html li 의 id를 객체로 전달받은 값(newToDoObjObj)의 id(date.now)로 지정
     const span=document.createElement("span");
-    span.innerText=newToDo.text;
+    span.innerText=newToDoObj.text;
     const button=document.createElement("button");
     button.innerText="X"
     button.addEventListener("click",deleteToDo)
@@ -37,13 +36,13 @@ function handleToDoSubmit(event){
         text:newToDo,
         id:Date.now(),//id로 각각 item을 구별
     };
-    toDos.push(newToDoObj);//newToDo(input에 타이핑 한 내용)을 배열toDos에 추가
+    toDos.push(newToDoObj);//객체 newToDoObj를 배열toDos에 추가
     paintToDo(newToDoObj);
     saveToDos();
 }
 toDoForm.addEventListener("submit",handleToDoSubmit);
 
-function sayHello(item){    //?item은 배열의 요소(타이핑한 값)인듯?
+function sayHello(item){    //?item은 배열의 요소(타이핑한 값)인듯? 매개변수가 꼭 item이 아니어도 됨
     console.log("this is the turn of ",item);
 }
 
